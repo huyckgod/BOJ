a5a5a4c44ff89f4ccb683228ec64a8ded5fb67c4
huyckgod <49019171+huyckgod@users.noreply.github.com>
2021-08-14 18:59:47 +0900

    2021-08-14

--- 03100/03190.cpp
+++ 03100/03190.cpp
@@ -4,7 +4,6 @@
 using namespace std;
 int t = 0;
 int dir = 100;
-//int dirn[4][2] = {{0,1},{1,0},{0,-1},{-1,0}};
 int dirn[4] = { 1,1,-1,-1 };
 int main()
 {
@@ -14,33 +13,21 @@ int main()
 	deque<vector<int>> bam;
 	bam.push_back(head);
 	char c;
-	scanf_s("%d", &n);
-	scanf_s("%d", &k);
+	scanf("%d", &n);
+	scanf("%d", &k);
 	vector<vector<int>>v(n, vector<int>(n, 0));
 	for (int i = 0; i < k; i++) {
-		scanf_s("%d %d", &a, &b);
+		scanf("%d %d", &a, &b);
 		v[a - 1][b - 1] = 1;
 	}
-	scanf_s("%d", &l);
+	scanf("%d", &l);
 	vector<pair<int, char>>vv;
 	for (int i = 0; i < l; i++) {
-		scanf_s("%d %c", &a, &c);
+		scanf("%d %c", &a, &c);
 		vv.push_back(pair<int, char>(a, c));
 	}
 	v[0][0] = 2;
 	while (1) {
-		if (t == 25) {
-			for (int i = 0; i < n; i++) {
-				for (int j = 0; j < n; j++) {
-					printf_s("%d", v[i][j]);
-				}
-				printf_s("\n");
-			}
-			for (int i = 0; i < bam.size(); i++) {
-				printf_s("%d %d\n", bam[i][0], bam[i][1]);
-			}
-			printf_s("%d",dir);
-		}
 		t++;
 		head[dir % 2] += dirn[dir % 4];
 		if (head[0] < 0 || head[1] < 0 || head[0] == n || head[1] == n || v[head[1]][head[0]] == 2) {
@@ -67,6 +54,6 @@ int main()
 			time1++;
 		}
 	}
-	printf_s("%d", t);
+	printf("%d", t);
 	return 0;
 }
