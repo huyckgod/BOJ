ce670278ccf47966598d092a596255bb7eefcb69
huyckgod <49019171+huyckgod@users.noreply.github.com>
2021-08-16 21:11:32 +0900

    2021-08-16

--- 12100/12100.cpp
+++ 12100/12100.cpp
@@ -1,28 +1,65 @@
 #include <iostream>
-#include <vector>
-#include <deque>
+#include<vector>
 using namespace std;
-long long arr[1000000];
-long long n;
-long long a, b;
-long long num = 0;
+int maxnum = 0;
+int a, b;
+vector<vector<int>> v(500, vector<int>(500, 0));
+void sol(int dir, int dep, int x, int y, int num) {
+	((dir % 2 == 0) ? x : y) += ((dir < 2) ? 1 : -1);
+	if (x < 0 || y < 0 || x == b || y == a) {
+		return;
+	}
+	num += v[y][x];
+	if (dep == 3) {
+		maxnum = (maxnum > num ? maxnum : num);
+		return;
+	}
+	sol(dir, dep + 1, x, y, num);
+	sol((dir + 3) % 4, dep + 1, x, y, num);
+	sol((dir + 1) % 4, dep + 1, x, y, num);
+	return;
+}
 int main()
 {
-	scanf("%d", &n);
-	for (int i = 0; i < n; i++) {
-		scanf("%d", &arr[i]);
-	}
-	scanf("%d %d", &a, &b);
-	for (int i = 0; i < n; i++) {
-		arr[i] -= a;
-		if (arr[i] < 0) {
-			arr[i] = 0;
+	for (int k = 0; k < 19; k++) {
+		cin >> a >> b;
+		for (int i = 0; i < a; i++) {
+			for (int j = 0; j < b; j++) {
+				cin >> v[i][j];
+			}
 		}
-		if (arr[i] % b != 0) {
-			num++;
+		for (int i = 0; i < a; i++) {
+			for (int j = 0; j < b; j++) {
+				sol(0, 1, j, i, v[i][j]);
+				sol(1, 1, j, i, v[i][j]);
+				sol(2, 1, j, i, v[i][j]);
+				sol(3, 1, j, i, v[i][j]);
+			}
 		}
-		num += arr[i] / b;
+		for (int i = 0; i < a - 1; i++) {
+			for (int j = 0; j < b - 2; j++) {
+				maxnum = maxnum > (v[i][j] + v[i][j + 1] + v[i][j + 2] + v[i + 1][j + 1]) ? maxnum : (v[i][j] + v[i][j + 1] + v[i][j + 2] + v[i + 1][j + 1]);
+			}
+		}
+		for (int i = 0; i < a - 1; i++) {
+			for (int j = 0; j < b - 2; j++) {
+				maxnum = maxnum > (v[i + 1][j] + v[i + 1][j + 1] + v[i + 1][j + 2] + v[i][j + 1]) ? maxnum : (v[i + 1][j] + v[i + 1][j + 1] + v[i + 1][j + 2] + v[i][j + 1]);
+			}
+		}
+		for (int i = 0; i < a - 2; i++) {
+			for (int j = 0; j < b - 1; j++) {
+				maxnum = maxnum > (v[i][j] + v[i + 1][j] + v[i + 2][j] + v[i + 1][j + 1]) ? maxnum : (v[i][j] + v[i + 1][j] + v[i + 2][j] + v[i + 1][j + 1]);
+			}
+		}
+		for (int i = 0; i < a - 2; i++) {
+			for (int j = 0; j < b - 1; j++) {
+				maxnum = maxnum > (v[i][j + 1] + v[i + 1][j + 1] + v[i + 2][j + 1] + v[i + 1][j]) ? maxnum : (v[i][j + 1] + v[i + 1][j + 1] + v[i + 2][j + 1] + v[i + 1][j]);
+			}
+		}
+		cout << maxnum << endl;
+		maxnum = 0;
 	}
-	printf("%d", num+n);
 	return 0;
 }
+
+
---
+++ 14500/14500.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include<vector>
+using namespace std;
+int maxnum = 0;
+int a, b;
+vector<vector<int>> v(500, vector<int>(500, 0));
+void sol(int dir, int dep, int x, int y, int num) {
+	((dir % 2 == 0) ? x : y) += ((dir < 2) ? 1 : -1);
+	if (x < 0 || y < 0 || x == b || y == a) {
+		return;
+	}
+	num += v[y][x];
+	if (dep == 3) {
+		maxnum = (maxnum > num ? maxnum : num);
+		return;
+	}
+	sol(dir, dep + 1, x, y, num);
+	sol((dir + 3) % 4, dep + 1, x, y, num);
+	sol((dir + 1) % 4, dep + 1, x, y, num);
+	return;
+}
+int main()
+{
+	cin >> a >> b;
+	for (int i = 0; i < a; i++) {
+		for (int j = 0; j < b; j++) {
+			cin >> v[i][j];
+		}
+	}
+	for (int i = 0; i < a; i++) {
+		for (int j = 0; j < b; j++) {
+			sol(0, 1, j, i, v[i][j]);
+			sol(1, 1, j, i, v[i][j]);
+			sol(2, 1, j, i, v[i][j]);
+			sol(3, 1, j, i, v[i][j]);
+		}
+	}
+	for (int i = 0; i < a - 1; i++) {
+		for (int j = 0; j < b - 2; j++) {
+			maxnum = maxnum > (v[i][j] + v[i][j + 1] + v[i][j + 2] + v[i + 1][j + 1]) ? maxnum : (v[i][j] + v[i][j + 1] + v[i][j + 2] + v[i + 1][j + 1]);
+		}
+	}
+	for (int i = 0; i < a - 1; i++) {
+		for (int j = 0; j < b - 2; j++) {
+			maxnum = maxnum > (v[i + 1][j] + v[i + 1][j + 1] + v[i + 1][j + 2] + v[i][j + 1]) ? maxnum : (v[i + 1][j] + v[i + 1][j + 1] + v[i + 1][j + 2] + v[i][j + 1]);
+		}
+	}
+	for (int i = 0; i < a - 2; i++) {
+		for (int j = 0; j < b - 1; j++) {
+			maxnum = maxnum > (v[i][j] + v[i + 1][j] + v[i + 2][j] + v[i + 1][j + 1]) ? maxnum : (v[i][j] + v[i + 1][j] + v[i + 2][j] + v[i + 1][j + 1]);
+		}
+	}
+	for (int i = 0; i < a - 2; i++) {
+		for (int j = 0; j < b - 1; j++) {
+			maxnum = maxnum > (v[i][j + 1] + v[i + 1][j + 1] + v[i + 2][j + 1] + v[i + 1][j]) ? maxnum : (v[i][j + 1] + v[i + 1][j + 1] + v[i + 2][j + 1] + v[i + 1][j]);
+		}
+	}
+	cout << maxnum << endl;
+	return 0;
+}
